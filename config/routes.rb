8ced5981d1ccb3240b8090a617d37f61c234d6e3
Brisingr <mdavid.almiron@gmail.com>
2023-02-16 10:56:01 -0300

    Setup tokenscontroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+
   namespace :api, defaults: { format: :json } do 
     namespace :v1 do 
       resources :users, only: [:show, :create, :update, :destroy]
+      resources :tokens, only: [:create]
     end
   end
+  
 end
